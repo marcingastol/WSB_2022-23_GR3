f9a20d0e13001c2a87f959d8e01384cd2d3c3a04
imie nazwisko <imie.nazwisko@gmail.com>
2022-12-04 08:42:31 +0000

    add 6.py

---
+++ Python/6.py
@@ -0,0 +1,8 @@
+def menu(wine, entree, dessert="pudding"):
+    return {"wine": wine, "entree": entree, "dessert": dessert}
+
+#print(menu("merlot","chicken","cake"))
+
+#print(menu(entree="chicken",wine="merlot",dessert="cake"))
+
+print(menu("merlot","chicken","apple pie"))
