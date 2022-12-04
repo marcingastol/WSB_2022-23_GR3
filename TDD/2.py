d6d94c880e7cfaf7948887b03c6f8b8ca8e35563
imie nazwisko <imie.nazwisko@gmail.com>
2022-12-04 12:05:44 +0000

    change 2.py

--- TDD/2.py
+++ TDD/2.py
@@ -2,12 +2,15 @@ import unittest
 
 class Dodawanie(unittest.TestCase):
     def test_main(self):
-        result = addition(3,6)
-        assert result == 10
+        result = addition(3,6,2)
+        assert result == 11
 
 
-def addition(arg1, arg2):
-    return arg1 + arg2
+def addition(*args):
+    total = 0
+    for i in args:
+        total += i
+    return total
 
 if __name__ == "__main__":
     unittest.main()
